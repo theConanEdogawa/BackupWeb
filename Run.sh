4c4f5f53b75dff86330007d64297d50fc47ff890
Conan-One <61743295+theConanEdogawa@users.noreply.github.com>
2021-05-01 14:14:40 +0800

    add log

--- Run.sh
+++ Run.sh
@@ -13,3 +13,4 @@ backupoutputname=$databasename'_'$(date "+%Y%m%d%H%M%S")'.sql'
 /usr/bin/mysqldump -u$mysqlusername -p$mysqluserpassword $databasename > $backupoutputname
 sleep 5
 /usr/bin/zip -9r -P$zippassword $backupoutputname'.zip' $backupoutputname
+echo -e "$(date "+%Y.%m.%d %H:%M:%S") Backup done." >> $log
