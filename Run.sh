1e8a0bec9157d988b019d5ae0432700ab6a6d0fa
Conan-One <61743295+theConanEdogawa@users.noreply.github.com>
2021-05-01 17:18:19 +0800

    Third reconstruction

--- Run.sh
+++ Run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 log=/root/BackupWeb.log
-
 [[ ! -f $log ]] && { mkdir -p $(dirname $log) ; touch $log ; }
 
 zippassword='zpasswd'
-mysqlusername='dbu'
-mysqluesrpassword='dbp'
-databasename='dbn'
-backupoutputname=$databasename'_'$(date "+%Y%m%d%H%M%S")'.sql'                                                                                                                                                                               
+mysqlusername'mysqlusr'
+mysqluserpassword='mysqlpw'
+databasename='mysqldbn'
+export dboutputname=$databasename'_'$(date "+%Y%m%d%H%M%S")'.sql'
+finaloutput=$dboutputname'.zip'
 
-/usr/bin/mysqldump -u$mysqlusername -p$mysqluserpassword $databasename > $backupoutputname
-sleep 5
-/usr/bin/zip -9r -P$zippassword $backupoutputname'.zip' $backupoutputname
-echo -e "$(date "+%Y.%m.%d %H:%M:%S") Backup done." >> $log
+/usr/bin/mysqldump -u$mysqlusername -p$mysqluserpassword $databasename > '/root/'$dboutputname
+echo -e "$(date "+%Y.%m.%d %H:%M:%S") Export database $dboutputname successfully." >> $log
+/usr/bin/zip -9r -P$zippassword '/root/'$finaloutput '/root/'$dboutputname
+echo -e "$(date "+%Y.%m.%d %H:%M:%S") Encrypted and compressed backup-files $finaloutput successfully" >> $log
+/usr/bin/rclone copy --ignore-existing '/root/'$finaloutput 'oss:bucket' && /usr/bin/rclone copy --ignore-existing '/root/'$finaloutput 'cos:bucket'
+echo -e "$(date "+%Y.%m.%d %H:%M:%S") Uploaded backup-files $finaloutput successfully." >> $log
+rm -f '/root/'$dboutputname '/root/'$finaloutput
