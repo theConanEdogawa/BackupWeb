284f0e815768d60c0bb8e0561b7cd0acce23f2ec
Conan-One <61743295+theConanEdogawa@users.noreply.github.com>
2021-05-04 15:11:49 +0800

    Adjustment format

--- Run.sh
+++ Run.sh
@@ -13,18 +13,18 @@ webdir='/home/webdir'
 webzip='xxx_'$(date "+%Y%m%d%H%M%S")'.zip'
 
 if [[ $1 == -db ]] ; then
-	/usr/bin/mysqldump -u$mysqlusername -p$mysqluserpassword $databasename > '/root/'$dboutputname
-	echo -e "[DB] $(date "+%Y.%m.%d %H:%M:%S") Export database $dboutputname successfully." >> $log
-	/usr/bin/zip -9r -P$zippassword '/root/'$finaloutput '/root/'$dboutputname
-	echo -e "[DB] $(date "+%Y.%m.%d %H:%M:%S") Encrypted and compressed backup-files $finaloutput successfully" >> $log
-	/usr/bin/rclone copy --ignore-existing '/root/'$finaloutput 'oss:bucket' && /usr/bin/rclone copy --ignore-existing '/root/'$finaloutput 'cos:bucket'
-	echo -e "[DB] $(date "+%Y.%m.%d %H:%M:%S") Uploaded backup-files $finaloutput successfully." >> $log
-	rm -f '/root/'$dboutputname '/root/'$finaloutput
+  /usr/bin/mysqldump -u$mysqlusername -p$mysqluserpassword $databasename > '/root/'$dboutputname
+  echo -e "[DB] $(date "+%Y.%m.%d %H:%M:%S") Export database $dboutputname successfully." >> $log
+  /usr/bin/zip -9r -P$zippassword '/root/'$finaloutput '/root/'$dboutputname
+  echo -e "[DB] $(date "+%Y.%m.%d %H:%M:%S") Encrypted and compressed backup-files $finaloutput successfully" >> $log
+  /usr/bin/rclone copy --ignore-existing '/root/'$finaloutput 'oss:bucket' && /usr/bin/rclone copy --ignore-existing '/root/'$finaloutput 'cos:bucket'
+  echo -e "[DB] $(date "+%Y.%m.%d %H:%M:%S") Uploaded backup-files $finaloutput successfully." >> $log
+  rm -f '/root/'$dboutputname '/root/'$finaloutput
 elif [[ $1 == -wf ]] ; then
-	/usr/bin/zip -9r -P$zippassword '/root/'$webzip $webdir
-	echo -e "[Web] $(date "+%Y.%m.%d %H:%M:%S") Encrypted and compressed backup-files $webzip successfully" >> $log
-	/usr/bin/rclone copy --ignore-existing '/root/'$webzip 'oss:bucket' && /usr/bin/rclone copy --ignore-existing '/root/'$webzip 'cos:bucket'
-	echo -e "[Web] $(date "+%Y.%m.%d %H:%M:%S") Uploaded backup-files $webzip successfully." >> $log
-	rm -f '/root/'$webzip
+  /usr/bin/zip -9r -P$zippassword '/root/'$webzip $webdir
+  echo -e "[Web] $(date "+%Y.%m.%d %H:%M:%S") Encrypted and compressed backup-files $webzip successfully" >> $log
+  /usr/bin/rclone copy --ignore-existing '/root/'$webzip 'oss:bucket' && /usr/bin/rclone copy --ignore-existing '/root/'$webzip 'cos:bucket'
+  echo -e "[Web] $(date "+%Y.%m.%d %H:%M:%S") Uploaded backup-files $webzip successfully." >> $log
+  rm -f '/root/'$webzip
 else exit 1 ;
 fi
